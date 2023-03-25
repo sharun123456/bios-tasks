0d6b6ffc12395afcd746db05168221c16f41aa8b
sharun123456 <69411242+sharun123456@users.noreply.github.com>
2023-03-25 16:30:28 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,79 @@
+public class Student {
+    private String name;
+    private int id;
+    private String[] courses;
+
+    public Student(String name,int id){
+        this.name = name;
+        this.id = id;
+        this.courses = new String[0];
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public String[] getcourses(){
+        return this.courses;
+    }
+    
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setId(int newId){
+        this.id = newId;
+    }
+    public void setcourses(String[] courses){
+        this.courses = courses;
+    }
+    public void addCourse(String course) {
+        String[] newCourses = new String[this.courses.length + 1];
+        for (int i = 0; i < this.courses.length; i++) {
+            newCourses[i] = this.courses[i];
+        }
+        newCourses[newCourses.length - 1] = course;
+        this.courses = newCourses;
+    }
+     public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.id);
+        for (String course : this.courses) {
+            System.out.print(course);
+        }
+        System.out.println();
+    }
+}
+
+class Graduatestudent extends Student {
+    private String researcharea;
+    
+    public Graduatestudent(String name,int id,String researcharea){
+        super(name,id);
+        this.researcharea = researcharea;
+    }
+    
+    public String getReasercharea(){
+        return this.researcharea;
+    }
+    public void setReasercharea(String researharea){
+        this.researcharea = researcharea;
+    }
+    public void printinfo(){
+        super.printInfo();
+        System.out.println(this.researcharea);
+    }
+}
+
+class main{
+    public static void main(String[] args) {
+        Student s1 = new Student("sharun", 22029);
+        s1.addCourse("java");
+        s1.addCourse("python");
+        s1.printInfo();
+
+        Graduatestudent s2 = new Graduatestudent("nani", 22016, "Computer Science");
+        s2.addCourse("DBMS");
+        s2.printInfo();
+    }
+}
